478def62a43bf15af6b96413afb99e9d55b4a777
GodblessMe2 <oshindeinde@gmail.com>
2022-10-31 18:18:17 +0100

    pycodestyle test

--- models/amenity.py
+++ models/amenity.py
@@ -2,11 +2,12 @@
 """Defines the user Amenity"""
 from models.base_model import BaseModel
 
+
 class Amenity(BaseModel):
    """Represents a Amenity of the AirBnB project
-      Attributes:
-         name (str): The name of the Amenity
-         amenity_place_id (string): The Place ID
+   Attributes:
+      name (str): The name of the Amenity
+      amenity_place_id (string): The Place ID
    """
 
    name = ""
