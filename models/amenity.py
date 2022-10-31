c092561a862331d1de45b22ae6fafe934480dc54
GodblessMe2 <oshindeinde@gmail.com>
2022-10-31 17:49:04 +0000

    pycodestyle for amenity

--- models/amenity.py
+++ models/amenity.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel
 
 class Amenity(BaseModel):
-   """Represents a Amenity of the AirBnB project
+    """Represents a Amenity of the AirBnB project
       Attributes:
          name (str): The name of the Amenity
          amenity_place_id (string): The Place ID
