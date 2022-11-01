f84c9ce9106f2a5d4819bddeb4f7ffa6ebbd91e0
GodblessMe2 <oshindeinde@gmail.com>
2022-11-02 00:41:35 +0100

    correction

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -32,18 +32,19 @@ class TestStateDoc(unittest.TestCase):
         """test for method docstring"""
         for func in self.amenity:
             with self.subTest(function=func):
-                 self.assertIsNot(
+                self.assertIsNot(
                         func[1].__doc__,
                         None,
-                        "{:s} method needs a doctsring".format(func[0])
+                        "{:s} method needs a docstring".format(func[0])
                         )
                 self.assertTrue(
                         len(func[1].__doc__) > 1,
                         "{:s} method needs a docstring".format(func[0]))
-
+            
 class TestAmenity(unittest.TestCase):
+
     """ Test amenity"""
-        def test_is_subclass(self):
+    def test_is_subclass(self):
         """Test amenity is a subclass of BaseModel"""
         amenity = Amenity()
         self.assertIsInstance(amenity, BaseModel)
