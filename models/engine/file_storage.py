d8ef51f1189baf9d3a79dc898a70923caffbba1c
vivian347 <vkinyua001@gmail.com>
2022-11-02 23:37:58 +0300

    pycode

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,9 +12,9 @@ from models.place import Place
 from models.review import Review
 
 
-classes = {"BaseModel" : BaseModel, "User" : User, "State" : State,
-           "City" : City, "Amenity" : Amenity, "Place" : Place,
-           "Review" : Review}
+classes = {"BaseModel":BaseModel, "User":User, "State":State,
+           "City":City, "Amenity":Amenity, "Place":Place,
+           "Review":Review}
 
 
 class FileStorage:
